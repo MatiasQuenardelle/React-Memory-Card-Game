c69591912f9932ae74d5ef4d6bdddb43d6770afe
Matias Quenardelle <matiasquenardelle@gmail.com>
2023-12-26 21:14:02 -0300

    Game Basic Functioning is Set

--- React-Memory-Card-Game/src/memoryGameApp.jsx
+++ React-Memory-Card-Game/src/memoryGameApp.jsx
@@ -5,6 +5,8 @@ import Pokemons from "./fetchPokemons"
 
 export default function MemoryGameApp() {
   const [pokemons, setPokemons] = useState([])
+  const [clickedPokemons, setClickedPokemons] = useState(new Set())
+  const [score, setScore] = useState(0)
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -31,26 +33,86 @@ export default function MemoryGameApp() {
     console.log(pokemons)
   }, [pokemons])
 
-  function handleClickedCard(e) {
+  function handleMouseMove(e) {
+    const card = e.currentTarget
+
+    const { left, top, width, height } = card.getBoundingClientRect()
+    const x = e.clientX - left
+    const y = e.clientY - top
+
+    const rotateX = (25 * (y - height / 2)) / height
+    const rotateY = (25 * (x - width / 2)) / width
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+  }
+
+  function handleMouseLeave(e) {
+    const card = e.currentTarget
+
+    card.style.transform = "none"
+  }
+
+  function handleStart() {
+    setPokemons(shufflePokemons([...pokemons]))
+  }
+
+  const shufflePokemons = (array) => {
+    let currentIndex = array.length,
+      randomIndex
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ]
+    }
+
+    return array
+  }
+
+  const handleCardClick = (e) => {
     const clickedCard = e.target.closest(".card")
     if (!clickedCard) return
-    clickedCard.remove()
+    const pokemonId = clickedCard.getAttribute("data-pokemon-id")
+    if (clickedPokemons.has(pokemonId)) {
+      alert("You clicked the same pokemon twice ! Game Over")
+      setScore(0)
+      setClickedPokemons(new Set())
+      setPokemons(shufflePokemons([...pokemons]))
+    } else {
+      setClickedPokemons(new Set(clickedPokemons.add(pokemonId)))
+      setScore((prevScore) => prevScore + 1)
+      setPokemons(shufflePokemons([...pokemons]))
+    }
   }
 
   return (
     <>
-      <div className="header"></div>
+      <div className="header">
+        <h2>Pokemon Memory Game</h2>
+        <button type="button" className="start" onClick={handleStart}>
+          START
+        </button>
+        <div className="score">
+          <p>Score:</p>
+          {score}
+        </div>
+      </div>
       <div className="cardsGrid">
         {pokemons.map((pokemon) => {
           return (
             <div
               className="card"
-              id="card1"
               key={pokemon.id}
-              onClick={handleClickedCard}
+              onClick={handleCardClick}
+              onMouseMove={handleMouseMove}
+              onMouseLeave={handleMouseLeave}
+              data-pokemon-id={pokemon.id}
             >
-              {pokemon.name}
-              <img src={pokemon.sprites.back_default} alt="" />
+              <h4>{pokemon.name.toUpperCase()}</h4>
+              <img src={pokemon.sprites.front_default} alt="" />
             </div>
           )
         })}
